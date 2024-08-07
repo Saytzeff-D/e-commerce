6896be12c3d5b3e7ae34c46912ef5db661cd844c
David Atanda <ololadedavid15@gmail.com>
2024-08-07 18:14:03 +0100

    feat: middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const authenticate = (req, res, next)=>{
     const splitJWT = req.headers.authorization
     if (splitJWT) {
-        const token = splitJWT.split(' ')[1]
+        const token = splitJWT.split(' ')[1]        
         jwt.verify(token, process.env.JWT_SECRET, (err, data)=>{
             if (err) {                
                 res.status(300).json({message: 'Expired Access Token'})
